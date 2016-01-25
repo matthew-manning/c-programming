cc598393921c4d91b110e63150ca49be85444dbf
matthew manning <mbhmanning@gmail.com>
2016-01-25 15:36:55 +1300

    regular commit

--- fileWrite.c
+++ fileWrite.c
@@ -3,5 +3,21 @@
 
 int main(void)
 {
+	FILE * CHECK = fopen("someFile.txt", "r");
+	if(CHECK)//seeing if file already exists
+	{
+		printf("warning, file already exists\n...over writing\n");
+	}
+	fclose(CHECK);
 	
+	FILE * WRITE_FILE = fopen("someFile.txt", "w");
+	if(!WRITE_FILE)//check handle okay
+	{
+		printf("failed to open/create file\n");
+	}
+	else//writing 
+	{
+	
+		
+	}
 }
